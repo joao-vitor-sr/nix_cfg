395e19b7c7cee6e31b94665518e614f0654091a1
Joao Vitor Silveira Ribeiro <joaovitorsrs7@gmail.com>
2023-10-17 22:32:48 -0300

    new syncthing config

--- pkgs/syncthing.nix
+++ pkgs/syncthing.nix
@@ -7,18 +7,24 @@
       dataDir = "/home/jvsr/dcs";
       overrideDevices = true;
       overrideFolders = true;
-      settings = {
-        devices = {
-          "smartphone" = {
-            id =
-              "FHMPUPO-E2IK7PZ-B6EGDX3-CLFC7ZS-H5HCCW4-IJRF3V3-SFYL3PN-LST2YQG";
-          };
+      devices = {
+        "home_computer" = {
+          id =
+            "TFNFXWI-AGLJNC7-4BFAWNJ-NNGLTVK-AN3PZGS-EL6PPNM-J4F6222-BZSL4AP";
         };
-        folders = {
-          "Obsidian Notes" = {
-            path = "/home/jvsr/dcs/obsidian/";
-            devices = [ "smartphone" ];
-          };
+        "smartphone" = {
+          id =
+            "FHMPUPO-E2IK7PZ-B6EGDX3-CLFC7ZS-H5HCCW4-IJRF3V3-SFYL3PN-LST2YQG";
+        };
+      };
+      folders = {
+        "Obsidian Notes" = {
+          path = "/home/jvsr/dcs/obsidian/";
+          devices = [ "smartphone" "home_computer" ];
+        };
+        "Wallpapers" = {
+          path = "/home/jvsr/pcs/bgs/";
+          devices = [ "home_computer" ];
         };
       };
     };
